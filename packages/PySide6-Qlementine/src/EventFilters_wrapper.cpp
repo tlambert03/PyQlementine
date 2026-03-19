e658bb6d8872ad92b01ed41eae89a04391789e63
Talley Lambert <talley.lambert@gmail.com>
2026-03-19 18:45:16 -0400

    refactor: replace EventFilters_wrapper with EventFilters in CMakeLists and pyproject.toml

--- packages/PySide6-Qlementine/src/EventFilters_wrapper.cpp
+++
@@ -1,4 +0,0 @@
-// Wrapper to add missing QCoreApplication include for qlementine's EventFilters.
-// The MenuEventFilter.hpp uses QCoreApplication::sendEvent without including it.
-#include <QCoreApplication>
-#include "style/EventFilters.cpp"
